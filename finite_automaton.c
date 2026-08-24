72926d243caa55a34ac273c79e35f15f19d4e59b
Danik2395 <daniil.kalenovich@gmail.com>
2026-08-24 13:49:19 +0300

    feat: add pressed state placing to finite

--- finite_automaton.c
+++ finite_automaton.c
@@ -6,16 +6,43 @@
 
 void finite_event(global_state_t* gs, const internal_event_t* ev)
 {
+    uint64_t time = get_time_ms();
+    gs->prev_key_time_ms = time;
+
+    pressed_state_t* key_state = &gs->pressed_state[ev->keycode];
+
+    bool key_active = ev->keystroke ? true : false;
+    key_state->active = key_active;
+
+    if (ev->key_type > LAST_TO_SEND_KEY_TYPE)
+    {
+	if (!key_active) return;
+
+	// if (ev->key_type == LAYER)
+	// {
+	key_state->key_send = 0;
+	key_state->layer_held = ev->layer;
+	key_state->time_send = time;
+	// }
+
+	// ...
+
+	return;
+    }
+
+    if (key_active)
+    {
+	key_state->key_send = ev->keycode;
+	key_state->layer_held = 0;
+	key_state->time_send = time;
+    }
+
     struct input_event raw_event = {
 	.code = ev->keycode,
 	.type = EV_KEY,
 	.value = ev->keystroke,
 	.time = {0, 0}
     };
-
-    gs->prev_key_time_ms = get_time_ms();
-
-    if (ev->key_type > LAST_TO_SEND_KEY_TYPE) return;
 
     (void)fwrite(&raw_event, sizeof(raw_event), 1, stdout);
 }
