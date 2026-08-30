8a3eff165005b17dc54ebc4f427d3b96a7559c2f
Danik2395 <daniil.kalenovich@gmail.com>
2026-08-30 14:47:59 +0300

    feat(test): add test to test test

--- test/sequence.py
+++ test/sequence.py
@@ -2,19 +2,49 @@ from t_types import TestUnit, InputEvent, LogicSeqUnit
 
 logic_sequences: list[LogicSeqUnit] = [
         LogicSeqUnit(
-            name= ,
+            name= "Test sequence",
             write_seq= [
-                TestUnit(InputEvent(seconds= , useconds= , ev_type= , code= , value= ),
-                         ),
+                TestUnit(InputEvent(seconds= 0, useconds= 0, ev_type= 1, code= 45, value= 1),
+                         0),
+                TestUnit(InputEvent(seconds= 0, useconds= 10000, ev_type= 0, code= 0, value= 0),
+                         10),
+                TestUnit(InputEvent(seconds= 0, useconds= 50000, ev_type= 1, code= 45, value= 0),
+                         50),
+                TestUnit(InputEvent(seconds= 0, useconds= 60000, ev_type= 0, code= 0, value= 0),
+                         60),
+
+                TestUnit(InputEvent(seconds= 0, useconds= 100000, ev_type= 1, code= 21, value= 1),
+                         100),
+                TestUnit(InputEvent(seconds= 0, useconds= 110000, ev_type= 0, code= 0, value= 0),
+                         110),
+                TestUnit(InputEvent(seconds= 0, useconds= 150000, ev_type= 1, code= 21, value= 0),
+                         150),
+                TestUnit(InputEvent(seconds= 0, useconds= 160000, ev_type= 0, code= 0, value= 0),
+                         160),
                 ],
-            read_seq = [
-                TestUnit(InputEvent(seconds= , useconds= , ev_type= , code= , value= ),
-                         ),
+            target_seq = [
+                TestUnit(InputEvent(seconds= 0, useconds= 0, ev_type= 1, code= 21, value= 1),
+                         0),
+                TestUnit(InputEvent(seconds= 0, useconds= 10000, ev_type= 0, code= 0, value= 0),
+                         10),
+                TestUnit(InputEvent(seconds= 0, useconds= 50000, ev_type= 1, code= 21, value= 0),
+                         50),
+                TestUnit(InputEvent(seconds= 0, useconds= 60000, ev_type= 0, code= 0, value= 0),
+                         60),
+
+                TestUnit(InputEvent(seconds= 0, useconds= 100000, ev_type= 1, code= 21, value= 1),
+                         100),
+                TestUnit(InputEvent(seconds= 0, useconds= 110000, ev_type= 0, code= 0, value= 0),
+                         110),
+                TestUnit(InputEvent(seconds= 0, useconds= 150000, ev_type= 1, code= 21, value= 0),
+                         150),
+                TestUnit(InputEvent(seconds= 0, useconds= 160000, ev_type= 0, code= 0, value= 0),
+                         160),
                 ]
             ),
         ]
 
-test_bad_macro_seq: list[TestUnit] = [
-        TestUnit(InputEvent(seconds= , useconds= , type= , code= , value= ),
-                 ),
-        ]
+# test_bad_macro_seq: list[TestUnit] = [
+#         TestUnit(InputEvent(seconds= , useconds= , type= , code= , value= ),
+#                  ),
+#         ]
