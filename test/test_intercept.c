18dffb2083778b369e819faeea2c5edc32f0849a
Danik2395 <daniil.kalenovich@gmail.com>
2026-08-30 21:43:30 +0300

    feat(test): add bad_byte test

--- test/test_intercept.c
+++ test/test_intercept.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <linux/input.h>
 
-int main(void) {
+int main(void)
+{
     setbuf(stdin, NULL), setbuf(stdout, NULL);
 
     struct input_event event;
-    while (fread(&event, sizeof(event), 1, stdin) == 1) {
+    while (1)
+    {
+        ssize_t bytes_read = read(STDIN_FILENO, &event, sizeof(event));
+
+        if (bytes_read != sizeof(event)) break;
+        else if (bytes_read == -1) break;
+
+
         if (event.type == EV_KEY && event.code == KEY_X)
             event.code = KEY_Y;
 
         fwrite(&event, sizeof(event), 1, stdout);
     }
+    fprintf(stderr, "bytes: 555\n");
 }
